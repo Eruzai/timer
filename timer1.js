50e9853945bfd95130daf52765d20a28afd9b271
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-17 10:52:41 -0800

    added timer functionality to beep after x seconds given by command line args

--- timer1.js
+++ timer1.js
@@ -1,3 +1,10 @@
 // timer beeps after specified amount(s) of time has passed. entered using command line arguments.
 const alarms = process.argv.slice(2);
-console.log(alarms);
+
+for (const alarm of alarms) {
+  const delay = alarm * 1000;
+  
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, delay)
+}
