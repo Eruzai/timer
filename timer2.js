46ca5ec54d1c743a6701e8085b99c79ffffc7ba4
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-18 13:22:01 -0800

    numbers set timers but logic on line 26 isn't correct

--- timer2.js
+++ timer2.js
@@ -13,17 +13,17 @@ stdin.on('data', (key) => {
 });
 
 
-const validateEntry = function(key) { // validates entry, returning false if NAN or not a number between 1 and 9
-  if (isNaN(Number(key)) || Number(key) < 0) {
-    console.log(`${key} isn't a valid entry. Please input a number between 1 and 9!`)
-    return false;
-  }
-};
+// const validateEntry = function(key) { // validates entry, returning false if NAN or not a number between 1 and 9
+//   if (isNaN(Number(key)) || Number(key) < 0) {
+//     console.log(`${key} isn't a valid entry. Please input a number between 1 and 9!`)
+//     return false;
+//   }
+// };
 
 stdin.on('data', (key) => {
   if (key === 'b') {
     process.stdout.write('\x07');
-  } else if (validateEntry(key)) {
+  } else if (0 < Number(key) < 9) {
     const entry = Number(key); // converts valid entry to a number rather than "9" for example
     const delay = entry * 1000; // converts entry into an amount of miliseconds
 
